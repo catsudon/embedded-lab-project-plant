3fd665b1a11edc1c4dc420222a55cb28c2cf298d
Mark Sat <sathanalao@hotmail.com>
2019-11-26 21:35:30 +0700

    added time display

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 const tem = firebase.firestore();
 const showhere = document.querySelector("h2");
+
+
 tem.collection('temp').get().then((snapshot)=>{
   snapshot.forEach(doc=>{
     console.log(doc.data());
@@ -16,3 +18,5 @@ $("button").click(function(){
 function showData(doc){
   showhere.innerHTML = doc.data().temp + " °C";
 }
+var date = new Date();
+document.getElementById("date").innerHTML = date.getHours()+":"+date.getMinutes()+":"+date.getSeconds()
